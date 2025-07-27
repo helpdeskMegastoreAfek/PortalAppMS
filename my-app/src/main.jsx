0f61a701a2dbca23371b17d116d1dced5cf1c2b1
helpdeskMegastoreAfek <helpdesk@megastoreafek.com>
2025-07-27 13:10:09 +0300

    add ui of service ticket

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './i18n';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// MUI Date Picker Imports - מוסיפים את השורות האלה
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+
+// Your App Components
 import EmployeePortal from './apps/mainbord';
 // import MealOrdering from './apps/MealOrdering';
 import BoxInventory from './apps/BoxInventory';
 import ITSupport from './apps/ITSupport';
-import Service from './apps/ServiceTickets';
+import Service from './apps/ServiceTickets'; // This component uses DatePicker
 import Login from './pages/Login';
 import AdminPanel from './apps/AdminPanel';
 import AdminBoxInventory from './apps/AdminBoxInventory';
@@ -17,19 +23,21 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<EmployeePortal />} />
-        {/* <Route path="/meal" element={<MealOrdering />} /> */}
-        <Route path="/boxes" element={<BoxInventory />} />
-        <Route path="/it" element={<ITSupport />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/adminBox" element={<AdminBoxInventory />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/invoice" element={<Invoice />} />
-        {/* <Route path="/adminMeal" element={<AdminMealOrdering />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<EmployeePortal />} />
+          {/* <Route path="/meal" element={<MealOrdering />} /> */}
+          <Route path="/boxes" element={<BoxInventory />} />
+          <Route path="/it" element={<ITSupport />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/adminBox" element={<AdminBoxInventory />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/invoice" element={<Invoice />} />
+          {/* <Route path="/adminMeal" element={<AdminMealOrdering />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </LocalizationProvider>
   </React.StrictMode>
 );
